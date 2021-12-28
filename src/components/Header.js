bf4964ec0cf1bb678ccd0e0af8cf9c2571168d8d
feduckiedev <frontendduckie@gmail.com>
2021-12-28 11:51:41 +0100

    responsiveness optimized

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ export default function Navbar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="sticky flex flex-wrap items-center justify-between px-2 py-3 bg-blue-500 mb-4 w-full top-0 left-0 right-0 ">
+      <nav className=" bg-slate-900 sticky flex flex-wrap items-center justify-between px-2 py-3 mb-4 w-full top-0 left-0 right-0 ">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a
@@ -21,7 +21,7 @@ export default function Navbar({ fixed }) {
               <img src={Duck} alt="Duck" className="w-48" />
             </a>
             <button
-              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent bg-transparent block lg:hidden outline-none focus:outline-none"
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent bg-transparent block lg:hidden outline-none focus:outline-none "
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
@@ -38,7 +38,7 @@ export default function Navbar({ fixed }) {
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75 "
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75 hover:text-blue-500 "
                   href="#about"
                 >
                   <span className="ml-2">About</span>
@@ -46,7 +46,7 @@ export default function Navbar({ fixed }) {
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75 hover:text-blue-500"
                   href="#socials"
                 >
                   <span className="ml-2">Socials</span>
@@ -54,7 +54,7 @@ export default function Navbar({ fixed }) {
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75 hover:text-blue-500"
                   href="#roadmap"
                 >
                   <span className="ml-2">Roadmap</span>
@@ -62,7 +62,7 @@ export default function Navbar({ fixed }) {
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75 hover:text-blue-500"
                   href="#tokenomics"
                 >
                   <span className="ml-2">Tokenomics</span>
@@ -70,7 +70,7 @@ export default function Navbar({ fixed }) {
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75 hover:text-blue-500"
                   href="#FAQs"
                 >
                   <span className="ml-2">FAQs</span>
@@ -78,7 +78,7 @@ export default function Navbar({ fixed }) {
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75 hover:text-blue-500"
                   href="#team"
                 >
                   <span className="ml-2">Team</span>
--- src/pages/body_sections/Socials.js
+++ src/pages/body_sections/Socials.js
@@ -1,23 +1,21 @@
 import React from "react";
-import { FaTwitter, FaTelegram, FaDiscord, FaInstagram } from "react-icons/fa";
+import { FaTwitter, FaTelegram, FaDiscord } from "react-icons/fa";
 import { IconContext } from "react-icons";
 
 const Socials = () => {
     return (
-        <section id="socials" className="bg-white flex h-32 justify-start content-center">
-            <IconContext.Provider value={{ size: '5em', style: { color: "#216aca", padding: "0.7em 0.7em 0.7em 0.7em", margin: "8px 0 0 0", } }}>
-                <a href="https://twitter.com/Duckiescoin" target="_blank" rel="noreferrer noopener" style={{ margin: "0 0 0 48px" }}>
-                    <FaTwitter />
+        <section id="socials" className="flex h-32 justify-start content-center">
+            <IconContext.Provider value={{ size: '5em', style: { padding: "0.7em 0.7em 0.7em 0.7em", margin: "8px 0 0 0", } }} className="icons">
+                <a href="https://twitter.com/Duckiescoin" target="_blank" rel="noreferrer noopener" style={{ margin: "0 0 0 48px" }} >
+                    <FaTwitter className="icon social" />
                 </a>
                 <a href="https://t.me/duckiescoin" target="_blank" rel="noreferrer noopener">
-                    <FaTelegram />
+                    <FaTelegram className="icon social" />
                 </a>
                 <a href="https://www.discord.gg/YXVZ5q22Qw" target="_blank" rel="noreferrer noopener">
-                    <FaDiscord />
+                    <FaDiscord className="icon social" />
                 </a>
-                <a href="https://instagram.com/duckiescoin" target="_blank" rel="noreferrer noopener">
-                    <FaInstagram />
-                </a>
+
             </IconContext.Provider>
 
 
