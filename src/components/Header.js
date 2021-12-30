5013777fd4a514fffafe9af54f7ce15f262970dc
feduckiedev <frontendduckie@gmail.com>
2021-12-31 00:43:13 +0100

    added pancakeswap option

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaBars } from "react-icons/fa";
 import Duck from "../assets/Duck.png";
+import PancakeSwap from "../assets/bunny-color.png"
 
 
 
@@ -11,16 +12,16 @@ export default function Navbar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className=" bg-slate-900 sticky flex flex-wrap items-center justify-between px-2 py-3 mb-4 w-full top-0 left-0 right-0 ">
+      <nav className=" bg-slate-900 sticky flex flex-wrap items-center justify-between px-2 py-3 mb-4 w-full top-0 left-0 right-0 z-50">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a
               className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
               href="/"
             >
-              <div className="flex" style={{alignItems: 'center'}}>
+              <div className="flex" style={{ alignItems: 'center' }}>
                 <img src={Duck} alt="Duck" className="w-24" />
-                <text style={{fontSize:'x-large'}}>Duckies</text>
+                <text style={{ fontSize: 'x-large' }}>Duckies</text>
               </div>
             </a>
             <button
@@ -47,14 +48,7 @@ export default function Navbar({ fixed }) {
                   <span className="ml-2">About</span>
                 </a>
               </li>
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75 hover:text-yellow-300"
-                  href="#socials"
-                >
-                  <span className="ml-2">Socials</span>
-                </a>
-              </li>
+
               <li className="nav-item">
                 <a
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75 hover:text-yellow-300"
@@ -82,9 +76,18 @@ export default function Navbar({ fixed }) {
               <li className="nav-item">
                 <a
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75 hover:text-yellow-300"
-                  href="#contact"
+                  href="#socials"
                 >
-                  <span className="ml-2">Contact</span>
+                  <span className="ml-2">Socials</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 ml-4 mr-2 flex items-center text-xs uppercase font-light leading-snug text-slate-900 hover:opacity-100 hover:text-yellow-300 border-white bg-blue-500 rounded-md"
+                  href="https://pancakeswap.finance" target="_blank" rel="noreferrer noopener"
+                >
+                  <img src={PancakeSwap} alt="Pancakeswap" className="w-4" />
+                  <span className="ml-2">Buy on pancakeswap</span>
                 </a>
               </li>
             </ul>
